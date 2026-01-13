6672b5820478015274edd8dba0a23b4e8ec1e63a
Prince Vishwakarma <prince.vishwakarma@ct-op.cwsystem.in>
2026-01-12 21:03:27 -0800

    test: promise methods

---
+++ promiseMethods/all.js
@@ -0,0 +1,30 @@
+function delay(value, ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(`Resolved: ${value}`);
+      resolve(value);
+    }, ms);
+  });
+}
+
+function fail(value, ms) {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      console.log(`Rejected: ${value}`);
+      reject(`${value} failed`);
+    }, ms);
+  });
+}
+
+Promise.all([
+  delay("B", 500),
+  fail("C", 200),
+   delay("A", 1000)
+])
+.then(results => {
+  console.log("Promise.all results:", results);
+})
+.catch(error => {
+  console.error("Promise.all error:", error);
+});
+
